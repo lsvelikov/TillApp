e76c1e52d1e039a339dd5a6c20d3d84fbc8964ba
lsvelikov <lubomirvelikov@yahoo.com>
2025-01-31 10:02:13 +0100

    tables html and css

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TableComponent } from "./table/table.component";
+import { NumbersService } from '../numbers.service';
+import { NumberComponent } from "../number/number.component";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-tables',
   standalone: true,
-  imports: [],
+  imports: [TableComponent, NumberComponent, ButtonComponent],
   templateUrl: './tables.component.html',
   styleUrl: './tables.component.css'
 })
 export class TablesComponent {
+  private numbersService = inject(NumbersService);
+  numbers = this.numbersService.numbers;
 
+  onClick(number: { id: string; value: string; }) {
+    console.log(number.value);
+    
+  }
+
+  onSubmit() {
+    
+  }
 }
