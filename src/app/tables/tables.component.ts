4551860a637a6ad8b46487b262a02fe3aa5518ea
lsvelikov <lubomirvelikov@yahoo.com>
2025-02-02 11:15:51 +0100

    fixing the tables component

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TableComponent } from "./table/table.component";
 import { NumbersService } from '../numbers.service';
 import { NumberComponent } from "../number/number.component";
@@ -14,13 +14,48 @@ import { ButtonComponent } from "../button/button.component";
 export class TablesComponent {
   private numbersService = inject(NumbersService);
   numbers = this.numbersService.numbers;
+  tableInput = signal<string>('Enter table number');
+  counter = 0;
+  currentNumber = signal<string>('');
+  error = signal<string>('');
+  
 
   onClick(number: { id: string; value: string; }) {
-    console.log(number.value);
+    if (number.value === '<') {
+      this.currentNumber.set(this.currentNumber().slice(0, - 1));
+      this.counter--;
+      
+      if (this.counter <= 0) {
+        this.tableInput.set('Enter table number');
+        this.counter = 0;
+      }
+    } else {
+      if ((number.value === '0' && this.counter === 0) || (number.value === '.' && this.counter === 0)) {
+        this.error.set("Table number cannot start with '0' or '.' !!!");
+        return;
+      }
+      if(+this.currentNumber() < 1000) {
+        if(+(this.currentNumber() + number.value) >= 1000 || this.currentNumber().length > 7) {
+          this.error.set("Table number cannot be bigger than 1000 and more than 7 digits !!!"); 
+          return;
+        }
+        if(this.currentNumber().includes('.') && number.value === '.') {
+          this.error.set("Table number cannot contain two decimal points !!!");
+          return;
+        }
+        this.currentNumber.set(this.currentNumber() + number.value);
+        this.counter++;
+      }
+    }
     
   }
 
   onSubmit() {
     
+  }
+
+  onClear() {
+    this.counter = 0;
+    this.currentNumber.set('');
   }
 }
